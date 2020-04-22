45ab85b0f8cbbad67d1091fd00ad9f98b541acab
Rafael Vieira <rafael.souzavieira@gmail.com>
2020-04-22 14:38:02 -0300

    Open new tab when click on card

--- src/main/resources/static/js/pipelineBox.js
+++ src/main/resources/static/js/pipelineBox.js
@@ -177,7 +177,7 @@ Vue.component("PipelineStage", {
           <div class="pl-2 pr-2 small rounded border border-secondary" v-bind:class="showRevision">{{ revisionId }}</div>
           <div class="p-1">
               <span v-bind:class="badgeType">
-                  <a class="text-light" v-bind:href="this.stage.externalExecutionUrl">{{ latestExecutionDate }}</a>
+                  <a class="text-light" v-bind:href="this.stage.externalExecutionUrl" target="_blank">{{ latestExecutionDate }}</a>
               </span>
           </div>
       </div>
