15d9bec7d0813de4a4a5dd7a80ab278f4a7c9ab5
Rafael Souza Vieira <rafael.souzavieira@gmail.com>
2020-05-04 20:20:49 -0300

    Filter pipelines by tag

--- src/main/java/de/codecentric/pipeline/AwsCodePipelineFacade.java
+++ src/main/java/de/codecentric/pipeline/AwsCodePipelineFacade.java
@@ -3,59 +3,82 @@ package de.codecentric.pipeline;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-import com.amazonaws.services.codepipeline.AWSCodePipeline;
-import com.amazonaws.services.codepipeline.model.*;
 import org.springframework.stereotype.Component;
+
+import com.amazonaws.services.codepipeline.AWSCodePipeline;
+import com.amazonaws.services.codepipeline.model.GetPipelineExecutionRequest;
+import com.amazonaws.services.codepipeline.model.GetPipelineExecutionResult;
+import com.amazonaws.services.codepipeline.model.GetPipelineRequest;
+import com.amazonaws.services.codepipeline.model.GetPipelineStateRequest;
+import com.amazonaws.services.codepipeline.model.GetPipelineStateResult;
+import com.amazonaws.services.codepipeline.model.ListPipelineExecutionsRequest;
+import com.amazonaws.services.codepipeline.model.ListPipelineExecutionsResult;
+import com.amazonaws.services.codepipeline.model.ListPipelinesRequest;
+import com.amazonaws.services.codepipeline.model.ListPipelinesResult;
+import com.amazonaws.services.codepipeline.model.ListTagsForResourceRequest;
+import com.amazonaws.services.codepipeline.model.PipelineSummary;
+import com.amazonaws.services.codepipeline.model.Tag;
 
 @Component
 public class AwsCodePipelineFacade {
-    private final AWSCodePipeline client;
+	private final AWSCodePipeline client;
+	private Tag tagFilter = null;
 
-    public AwsCodePipelineFacade (AWSCodePipeline awsCodePipeline) {
-        this.client = awsCodePipeline;
-    }
+	public AwsCodePipelineFacade (AWSCodePipeline awsCodePipeline) {
+		this.client = awsCodePipeline;
+		this.tagFilter = new Tag();
+		String key =  System.getenv("AWS_TAG_FILTER") != null ? System.getenv("AWS_TAG_FILTER") : "canais";
+		tagFilter.setKey(key);
+	}
 
-    public ListPipelinesResult getPipelineResults(String group) {
-        ListPipelinesResult result = client.listPipelines(new ListPipelinesRequest());
-        Predicate<PipelineSummary> filter = pipeline -> pipeline.getName().contains(group);
+	public ListPipelinesResult getPipelineResults(String group) {
+		ListPipelinesResult result = client.listPipelines(new ListPipelinesRequest());
 
-        result.setPipelines(result.getPipelines().stream().filter(filter)
-        .collect(Collectors.toList()));
+		if(!group.isEmpty()) {
+			tagFilter.setValue(group);
+			Predicate<PipelineSummary> filter = pipeline ->
+			this.client.listTagsForResource(new ListTagsForResourceRequest().withResourceArn(
+					this.client.getPipeline(new GetPipelineRequest().withName(pipeline.getName()))
+					.getMetadata().getPipelineArn())).getTags().contains(tagFilter);
 
-        return result;
-    }
+			result.setPipelines(result.getPipelines().stream().filter(filter)
+					.collect(Collectors.toList()));
+		}
 
-    public GetPipelineStateResult getPipelineStatus(String pipelineName) {
-        return client.getPipelineState(new GetPipelineStateRequest().withName(pipelineName));
-    }
+		return result;
+	}
 
-    public String getLatestCommitMessage(String pipelineName) {
-        String latestPipelineExecutionId = getLatestPipelineExecutionId(pipelineName);
-        GetPipelineExecutionResult pipelineExecution = getPipelineExecutionSummary(pipelineName, latestPipelineExecutionId);
-        return getLatestRevisionSummary(pipelineExecution);
-    }
+	public GetPipelineStateResult getPipelineStatus(String pipelineName) {
+		return client.getPipelineState(new GetPipelineStateRequest().withName(pipelineName));
+	}
 
-    private String getLatestPipelineExecutionId(String name) {
-        ListPipelineExecutionsResult pipelineExecutionsResult = getLatestPipelineExecutionResult(name);
-        return pipelineExecutionsResult.getPipelineExecutionSummaries().get(0).getPipelineExecutionId();
-    }
+	public String getLatestCommitMessage(String pipelineName) {
+		String latestPipelineExecutionId = getLatestPipelineExecutionId(pipelineName);
+		GetPipelineExecutionResult pipelineExecution = getPipelineExecutionSummary(pipelineName, latestPipelineExecutionId);
+		return getLatestRevisionSummary(pipelineExecution);
+	}
 
-    private String getLatestRevisionSummary(GetPipelineExecutionResult pipelineExecution) {
-        return pipelineExecution.getPipelineExecution()
-                .getArtifactRevisions()
-                .get(0)
-                .getRevisionSummary();
-    }
+	private String getLatestPipelineExecutionId(String name) {
+		ListPipelineExecutionsResult pipelineExecutionsResult = getLatestPipelineExecutionResult(name);
+		return pipelineExecutionsResult.getPipelineExecutionSummaries().get(0).getPipelineExecutionId();
+	}
 
-    private GetPipelineExecutionResult getPipelineExecutionSummary(String name, String latestPipelineExecutionId) {
-        return client.getPipelineExecution(
-                new GetPipelineExecutionRequest()
-                        .withPipelineExecutionId(latestPipelineExecutionId).withPipelineName(name));
-    }
+	private String getLatestRevisionSummary(GetPipelineExecutionResult pipelineExecution) {
+		return pipelineExecution.getPipelineExecution()
+				.getArtifactRevisions()
+				.get(0)
+				.getRevisionSummary();
+	}
 
-    private ListPipelineExecutionsResult getLatestPipelineExecutionResult(String name) {
-        return client.listPipelineExecutions(
-                new ListPipelineExecutionsRequest().withPipelineName(name).withMaxResults(1));
-    }
+	private GetPipelineExecutionResult getPipelineExecutionSummary(String name, String latestPipelineExecutionId) {
+		return client.getPipelineExecution(
+				new GetPipelineExecutionRequest()
+				.withPipelineExecutionId(latestPipelineExecutionId).withPipelineName(name));
+	}
+
+	private ListPipelineExecutionsResult getLatestPipelineExecutionResult(String name) {
+		return client.listPipelineExecutions(
+				new ListPipelineExecutionsRequest().withPipelineName(name).withMaxResults(1));
+	}
 
 }
